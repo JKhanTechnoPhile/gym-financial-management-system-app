cd916525cc3fb4e1686cac51a629861c65ca73bf
Javeed Khan H <javeedkhanh1987@gmail.com>
2022-08-23 16:40:33 +0530

    JWT Token based authentication API access Implemented

--- src/main/java/com/hiddu/gym/enterprise/config/SecurityConfig.java
+++ src/main/java/com/hiddu/gym/enterprise/config/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.hiddu.gym.enterprise.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.hiddu.gym.enterprise.security.CustomUserDetailService;
+import com.hiddu.gym.enterprise.security.JwtAuthenticationEntryPoint;
+import com.hiddu.gym.enterprise.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
 	
+	@Autowired
+	private JwtAuthenticationEntryPoint authenticationEntryPoint;
+	
+	@Autowired
+	private JwtAuthenticationFilter authenticationFilter;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -26,10 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.csrf()
 		.disable()
 		.authorizeHttpRequests()
+		.antMatchers("/api/v1/auth/login").permitAll()
 		.anyRequest()
 		.authenticated()
 		.and()
-		.httpBasic();
+		.exceptionHandling()
+		.authenticationEntryPoint(authenticationEntryPoint)
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Override
@@ -40,6 +58,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 
 }
---
+++ src/main/java/com/hiddu/gym/enterprise/controllers/AuthController.java
@@ -0,0 +1,61 @@
+package com.hiddu.gym.enterprise.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hiddu.gym.enterprise.payloads.JwtAuthRequest;
+import com.hiddu.gym.enterprise.payloads.JwtAuthResponse;
+import com.hiddu.gym.enterprise.security.JwtTokenHelper;
+
+@RestController
+@RequestMapping("/api/v1/auth/")
+public class AuthController {
+	
+	@Autowired
+	private JwtTokenHelper jwtTokenHelper;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<JwtAuthResponse> createToken(
+			@RequestBody JwtAuthRequest request
+			) throws Exception {
+		
+		this.authenticate(request.getUsername(), request.getPassword());
+		UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
+		String token = this.jwtTokenHelper.generateToken(userDetails);
+		
+		JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+		jwtAuthResponse.setToken(token);
+		
+		return ResponseEntity.ok(jwtAuthResponse);
+	}
+
+
+
+	private void authenticate(String username, String password) throws Exception {
+		
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		try {
+			this.authenticationManager.authenticate(authenticationToken);
+		} catch (BadCredentialsException e) {
+			throw new Exception("Invalid username or password..!!");
+		}
+	}
+
+}
