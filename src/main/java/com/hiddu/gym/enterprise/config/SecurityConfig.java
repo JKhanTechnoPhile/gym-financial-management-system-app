da0a604abf5e67778f12c4be11fe45009bdbf106
Javeed Khan H <javeedkhanh1987@gmail.com>
2022-08-25 02:37:59 +0530

    Added Swagger dependency to API documentation using swagger UI

--- src/main/java/com/hiddu/gym/enterprise/config/SecurityConfig.java
+++ src/main/java/com/hiddu/gym/enterprise/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.hiddu.gym.enterprise.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.hiddu.gym.enterprise.security.CustomUserDetailService;
 import com.hiddu.gym.enterprise.security.JwtAuthenticationEntryPoint;
@@ -21,8 +21,18 @@ import com.hiddu.gym.enterprise.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	public static final String[] PUBLIC_URLS = {
+			"/api/v1/auth/**",
+			"/v3/api-docs",
+			"/v2/api-docs",
+			"/swagger-resources/**",
+			"/swagger-ui/**",
+			"/webjars/**"
+	};
 	
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
@@ -40,7 +50,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.csrf()
 		.disable()
 		.authorizeHttpRequests()
-		.antMatchers("/api/v1/auth/**").permitAll()
+		.antMatchers(PUBLIC_URLS).permitAll()
+//		.antMatchers("/api/v1/auth/**").permitAll()
+//		.antMatchers("/v3/api-docs").permitAll()
 //		.antMatchers("/api/v1/auth/login").permitAll()
 //		.antMatchers(HttpMethod.GET).permitAll() //Allow all GET request without authentication
 		.anyRequest()
